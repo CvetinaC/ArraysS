bd9b83f18ed948dfe6a14463881f1269cecaa0a2
CvetinaC <35578703+CvetinaC@users.noreply.github.com>
2018-11-07 21:41:46 +0200

    Update Program.cs

--- ScaleFocus/Zadacha13/Program.cs
+++ ScaleFocus/Zadacha13/Program.cs
@@ -14,20 +14,20 @@ namespace Zadacha13
             int n = int.Parse(Console.ReadLine());
             int[] A = new int[n];
             int b = 6;
-            int min=0;
+            int min=int.MaxValue;
+            int index = 0;
             for (int i = 0; i < A.Length; i++)
             {
                 A[i] = int.Parse(Console.ReadLine());
             }
             for (int i = 0; i < A.Length; i++)
             {
-                min = A[i];
-                if (A[i] < min && A[i] > b)
+                if (A[i] > b && A[i]<= min)
                 {
                     min = A[i];
+                    Console.WriteLine("Element {0} with min value, which is bigger than b is {1} : ", i, min);
                 }
             }
-            Console.WriteLine("The element with min value, which is bigger than b is  : " + min);
         }
     }
 }
