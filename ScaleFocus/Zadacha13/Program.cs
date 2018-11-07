5dc6c451fb8555c4dc3060729e1d8a7b99fbcef9
Cvetina Cekina <cvetina.cekina97@gmail.com>
2018-11-07 19:49:43 +0200

    Cvetina
    Arrays project

---
+++ ScaleFocus/Zadacha13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n : ");
+            int n = int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            int b = 6;
+            int min=0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < A.Length; i++)
+            {
+                min = A[i];
+                if (A[i] < min && A[i] > b)
+                {
+                    min = A[i];
+                }
+            }
+            Console.WriteLine("The element with min value, which is bigger than b is  : " + min);
+        }
+    }
+}
